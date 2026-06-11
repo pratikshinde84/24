06bf101f5a1cac1acaee655c5eb0026f72be378e
Pratik <pratik216029@gmail.com>
2026-06-11 14:38:37 +0530

    demo

--- hello.java
+++ hello.java
@@ -1,7 +1,29 @@
 class hello{
+    public boolean lemonadeChange(int[] bills) {
+        int five=0,ten=0;
+        for(int i:bills){
+            if(i==5){
+                five++;
+            }else if(i==10){
+                if(five>=1)five--;
+                else{
+                    return false;
+                }
+            }else{
+                if(five>=1&&ten>=1){
+                    five--;
+                    ten--;
+                }else if(five>=3){
+                    five-=3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello,!");
-        System.out.println("How are you?"); 
-        
+        sout
     }
 }
