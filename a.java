8ff86acad321751a2049ca0a94e73597390f007e
Pratik <pratik216029@gmail.com>
2026-06-23 02:44:17 +0530

    done

--- a.java
+++ a.java
@@ -1,15 +1,15 @@
 class a{
     int majorityElement(int arr[]) {
-        int count=0,num=arr[0];
+        int count=1,num=arr[0];
         for(int i=1;i<arr.length;i++){
             if(num==arr[i]){
                 count++;
-                continue;
             }else{
                 count--;
             }
             if(count==0){
                 num=arr[i];
+                count=1;
             }
         }
         return num;
