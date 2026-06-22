a1980a1a4fd176d90cffdebcd3418170c1686bcd
Pratik <pratik216029@gmail.com>
2026-06-22 20:27:14 +0530

    done

--- trial.java
+++ trial.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class trial{
     public boolean canPlaceFlowers(int[] f, int n) {
         int count=0;
@@ -13,6 +17,50 @@ class trial{
         }
         return count>=n; 
     }
+    ArrayList<Integer> boundaryTraversal(Node root) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        ans.add(root.data);
+        leftBoundary(root.left,ans);
+        leafNodes(root,ans);
+        rightBoundary(root.right,ans);
+        return ans;
+    }
+    void leftBoundary(Node root, ArrayList<Integer> ans){
+        if(root==null||root.left==null&&root.right==null){
+            return;
+        }
+        ans.add(root.data);
+        if(root.left!=null){
+            leftBoundary(root.left,ans);
+        }else{
+            leftBoundary(root.right,ans);
+        }
+    }
+    void rightBoundary(Node root, ArrayList<Integer> ans){
+        if(root==null||root.left==null&&root.right==null){
+            return;
+        }
+        if(root.right!=null){
+            rightBoundary(root.right,ans);
+        }else{
+            rightBoundary(root.left,ans);
+        }
+        ans.add(root.data);
+    }
+    void leafNodes(Node root, ArrayList<Integer> ans){
+        if(root==null){
+            return;
+        }
+        if(root.left==null&&root.right==null){
+            ans.add(root.data);
+        }
+        leafNodes(root.left,ans);
+        leafNodes(root.right,ans);
+    }
+    
     public static void main(String[] args) {
         System.out.println(new trial().canPlaceFlowers(new int[]{1,0,0,0,1}, 1));
     } 
