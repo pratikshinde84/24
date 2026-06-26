07158b662754471059b10f2b1bf3c9f134f5f864
Pratik <pratik216029@gmail.com>
2026-06-26 22:47:44 +0530

    done

--- python/first.py
+++ python/first.py
@@ -1,10 +1,4 @@
-class A:
-    def __init__(self):
-        self.a = "pratik"
-    def __del__(self):
-        print("Destructor called, A deleted.")
-
-obj = A()
 for i in range(5):
-    print(obj.a)
-del obj
+    print(i)
+str="Hello, World!"
+print(str.upper())
