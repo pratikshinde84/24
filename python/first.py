9977c8902f4da3cf4e4a1a75ac8684f3b7e2ae6e
Pratik <pratik216029@gmail.com>
2026-06-27 22:19:27 +0530

    done

--- python/first.py
+++ python/first.py
@@ -1,4 +1,7 @@
-i=3
-while(i>0):
-    print(i)
-    i -= 1
+lst=[1,2,3,4,5]
+start,end=0,len(lst)-1
+while start<end:
+    lst[start],lst[end]=lst[end],lst[start]
+    start=start+1
+    end-=1
+print(lst)
