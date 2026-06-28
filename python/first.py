70354a6fedac55cc1ee788e5f62272100d5fefe4
Pratik <pratik216029@gmail.com>
2026-06-28 20:46:58 +0530

    done

--- python/first.py
+++ python/first.py
@@ -1,3 +1,7 @@
+import os
+
+print(os.getcwd())
+import math,random
 lst=[1,2,3,4,5]
 start,end=0,len(lst)-1
 while start<end:
@@ -5,3 +9,9 @@ while start<end:
     start=start+1
     end-=1
 print(lst)
+cards = [1, 2, 3, 4]
+
+random.shuffle(cards)
+print(math.gcd(12,14))
+
+print(cards)
