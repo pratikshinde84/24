af2c149e8e601281bacef73d62d6549ad9b34554
Pratik <pratik216029@gmail.com>
2026-07-12 03:03:57 +0530

    done

--- Pattern.java
+++ Pattern.java
@@ -1,13 +1,21 @@
 class Pattern{
-    static void print1(int n){
-        for(int i=n;i>0;i--){
-            for(int j=0;j<i;j++){
+    static void print1(int n) {
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
+    static void print(int n){
+        for(int i=5;i>0;i--){
+            for(int j=0;j<i;j++){;
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String ar[]){
-        print1(5);
+        print(5);
     }
 }
---
+++ out/production/New folder/python/first.py
@@ -0,0 +1,19 @@
+import os
+
+print(os.getcwd())
+import math,random
+lst=[1,2,3,4,5]
+start,end=0,len(lst)-1
+while start<end:
+    lst[start],lst[end]=lst[end],lst[start]
+    start=start+1
+    end-=1
+print(lst)
+
+
+cards = [1, 2, 3, 4]
+ 
+random.shuffle(cards)
+print(math.gcd(12,14))
+
+print(cards)
