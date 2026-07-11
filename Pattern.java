ce133349f0db73cfeeb82c8d629ea3fd56e2b713
Pratik <pratik216029@gmail.com>
2026-07-12 03:12:37 +0530

    done

--- Pattern.java
+++ Pattern.java
@@ -8,14 +8,28 @@ class Pattern{
         }
     }
     static void print(int n){
-        for(int i=5;i>0;i--){
+        for(int i=n;i>0;i--){
             for(int j=0;j<i;j++){;
                 System.out.print(j+1);
             }
             System.out.println();
         }
     }
+    static void print2(int n){
+        for(int i=n-1;i>=0;i--){
+            for(int spaces=0;spaces<i;i++){
+                System.out.print(" ");
+            }
+            for(int star=0;star<n-i;i++){
+                System.out.print("*");
+            }
+            for(int star=0;star<n-i-1;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String ar[]){
-        print(5);
+        print2(5);
     }
 }
