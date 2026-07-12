e5fd574299930383044a417af6d3ed48af1e89d1
Pratik <pratik216029@gmail.com>
2026-07-13 03:05:57 +0530

    with janhavi done

--- Pattern.java
+++ Pattern.java
@@ -44,8 +44,52 @@ class Pattern{
         }
     }
 
+    static void pattern1(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(j+1+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(i+1+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print(j+1+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String ar[]){
-        print2(5);
-        print3(5);
+        pattern5(5);
     }
 }
