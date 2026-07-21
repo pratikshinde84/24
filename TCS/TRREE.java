0e8b4dbd7458658b1ad163cb47a6dd9667b5d886
Pratik <pratik216029@gmail.com>
2026-07-22 04:32:47 +0530

    tcs

--- TCS/TRREE.java
+++ TCS/TRREE.java
@@ -1,5 +1,37 @@
 package TCS;
 
-public class TRREE {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class TRREE {
+    Node root;
+    class Node{
+        Node left,right;
+        int data;
+        Node(int data){
+            this.data=data;
+        }
+    }
+
+    void build(int ar[]){
+        Queue<Node> q=new LinkedList<>();
+        int i=0;
+        root=new Node(ar[i++]);
+        q.add(root);
+        while(!q.isEmpty()){
+            Node curr=q.poll();
+            if(i<ar.length){
+                curr.left=new Node(ar[i++]);
+                q.add(curr.left);
+            }
+            if(i<ar.length){
+                curr.right=new Node(ar[i++]);
+                q.add(curr.right);
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
