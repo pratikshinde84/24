343f19aa39eb4235d4f1380a445190923806c820
Pratik <pratik216029@gmail.com>
2026-09-19 21:42:58 +0530

    done

--- TCS/Janhavi.java
+++ TCS/Janhavi.java
@@ -5,6 +5,13 @@ public class Janhavi {
         int data;
         Node next;
     }
+
+    Node head;
+    // ata aplyla jar ekhadya node la point karaycha ahe kiva
+    // first number cha node store karaycha ahe tr tyasathi
+    // reference variable suddha Node type cha hava na ????????
+
+
     // ha class ahe node ata apan yache object banavle ki
     // pratek object aaplya linked list madhla ek ek node banel ok???
 
---
+++ lc/Pattern.java
@@ -0,0 +1,10 @@
+package lc;
+
+public class Pattern {
+    void pattern(int n){
+        for(int i=0;i<)
+    }
+    public static void main(String[] args) {
+
+    }
+}
