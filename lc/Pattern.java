f0e66dbcba80c8ca95d70decedabca499ea2025b
Pratik <pratik216029@gmail.com>
2026-09-20 19:32:38 +0530

    done

--- lc/Pattern.java
+++ lc/Pattern.java
@@ -2,9 +2,15 @@ package lc;
 
 public class Pattern {
     void pattern(int n){
-        for(int i=0;i<)
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
-
+        Pattern pt=new Pattern();
+        pt.pattern(5);
     }
 }
